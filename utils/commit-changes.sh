9aa0428ccc9a7c006e9c641f38ce44b83ebd0be3
Scott Doxey <neogeek0101@gmail.com>
2018-08-22 21:15:53 -0400

    Added git config to commit changes script.

--- utils/commit-changes.sh
+++ utils/commit-changes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.email "$GITHUB_USER_EMAIL"
+git config --global user.name "$GITHUB_USER_NAME"
+
 git add data/editor-installers.json
 git commit -m "Updated editor-installers.json [skip ci]"
 git push "https://$GITHUB_TOKEN@github.com/neogeek/unity-ci-tools"
