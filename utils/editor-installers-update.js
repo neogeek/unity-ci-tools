b831c7fd4f2c9916805449688d82c76f3ecdde6c
Scott Doxey <neogeek0101@gmail.com>
2019-04-20 16:17:13 -0400

    Updated editor installation script to work with new dom structure.

--- utils/editor-installers-update.js
+++ utils/editor-installers-update.js
@@ -5,31 +5,36 @@ const { fetch } = require('raspar');
 const { JSDOM } = require('jsdom');
 
 const UNITY_VERSION_PATTERN = /[0-9]{1,4}\.[0-9]+\.[0-9a-z]+/;
+const UNITY_EDITOR_PATTERN = /Unity Editor/;
 
 fetch('https://unity3d.com/get-unity/download/archive').then(({ body }) => {
     const { document } = new JSDOM(body).window;
     const editorInstallers = [].slice
-        .call(
-            document.querySelectorAll(
-                '.archive .row .options a[href*="EditorInstaller"]'
-            )
-        )
-        .map(elem => elem.getAttribute('href'))
-        .reduce((acc, url) => {
-            const version = url.match(UNITY_VERSION_PATTERN)[0];
+        .call(document.querySelectorAll('.version a[href*="unityhub"]'))
+        .filter(elem => elem.getAttribute('href').match(UNITY_VERSION_PATTERN))
+        .reduce((acc, elem) => {
+            const version = elem
+                .getAttribute('href')
+                .match(UNITY_VERSION_PATTERN)[0];
+
+            const links = [].slice
+                .call(elem.parentNode.parentNode.querySelectorAll('.options a'))
+                .filter(elem => elem.innerHTML.match(UNITY_EDITOR_PATTERN))
+                .map(elem => elem.getAttribute('href'));
 
             if (!acc[version]) {
                 acc[version] = {};
             }
 
-            if (url.match(/MacEditor/)) {
-                acc[version].mac = url;
-            } else if (url.match(/Windows64/)) {
-                acc[version].win64 = url;
-            } else if (url.match(/Windows32/)) {
-                acc[version].win32 = url;
-            }
-
+            links.map(url => {
+                if (url.match(/.(dmg|pkg)$/)) {
+                    acc[version].mac = url;
+                } else if (url.match(/Windows64/)) {
+                    acc[version].win64 = url;
+                } else if (url.match(/Windows32/)) {
+                    acc[version].win32 = url;
+                }
+            });
             return acc;
         }, {});
     writeFileSync(
