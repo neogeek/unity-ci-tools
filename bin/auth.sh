33258dc8ce479a25c62ee5ca683de1376bd93a38
neogeek <neogeek0101@gmail.com>
2019-08-12 23:58:24 -0400

    Added EVENT_NOKQUEUE fix to each script.

--- bin/auth.sh
+++ bin/auth.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://github.com/tmux/tmux/issues/475#issuecomment-231527324
+export EVENT_NOKQUEUE=1
+
 UNITY_APPLICATION=$(find /Applications/Unity -name Unity.app | head -1)
 
 "${UNITY_APPLICATION}/Contents/MacOS/Unity" \
