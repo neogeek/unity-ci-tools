551ca6b60a7a4ac990e020c1b23f4687f48e1ac4
neogeek <neogeek0101@gmail.com>
2019-12-05 23:09:36 -0500

    Test with version of Unity project was built with.

--- bin/test.sh
+++ bin/test.sh
@@ -3,7 +3,30 @@
 # https://github.com/tmux/tmux/issues/475#issuecomment-231527324
 export EVENT_NOKQUEUE=1
 
-UNITY_APPLICATION=$(find /Applications/Unity -name Unity.app | head -1)
+findunity() {
+
+    local UNITY_INSTALLS
+    local PROJECT_VERSION
+    local UNITY_INSTALL_VERSION_MATCH
+    local UNITY_INSTALL_LATEST
+
+    UNITY_INSTALLS=$(find /Applications/Unity -name "Unity.app" | sort -r)
+
+    if [ -f "ProjectSettings/ProjectVersion.txt" ]; then
+
+        PROJECT_VERSION=$(grep -o -E "[0-9]+\.[0-9]+\.[0-9]+[a-z][0-9]+" "ProjectSettings/ProjectVersion.txt" | head -1)
+
+        UNITY_INSTALL_VERSION_MATCH=$(grep "${PROJECT_VERSION}" <<< "${UNITY_INSTALLS}")
+
+    fi
+
+    UNITY_INSTALL_LATEST=$(head -1 <<< "${UNITY_INSTALLS}")
+
+    UNITY_APPLICATION=$([ -n "${UNITY_INSTALL_VERSION_MATCH}" ] && echo "${UNITY_INSTALL_VERSION_MATCH}" || echo "${UNITY_INSTALL_LATEST}")
+
+}
+
+findunity
 
 UNITY_VERSION=$(defaults read "${UNITY_APPLICATION}/Contents/Info.plist" CFBundleVersion)
 
